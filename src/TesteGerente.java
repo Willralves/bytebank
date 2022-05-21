67b658ce7ed94ce41a4cb169b75736a9429251da
William Robert da Veiga Alves <wra.profissional@gmail.com>
2022-05-21 19:21:54 -0300

    Segundo commit bytebank

--- src/TesteGerente.java
+++ src/TesteGerente.java
@@ -4,12 +4,20 @@ public class TesteGerente {
 	public static void main(String[] args) {
 		
 		Gerente gerente1 = new Gerente();
+		Funcionario funcionario1 = new Funcionario();
 		
 		gerente1.setNome("William");
 		gerente1.setSalario(7500.00);
 		
+
+		funcionario1.setNome("William");
+		funcionario1.setSalario(7500.00);
+		
 		System.out.println(gerente1.getNome());
 		System.out.println(gerente1.getSalario());
+		
+		System.out.println(funcionario1.getNome());
+		System.out.println(funcionario1.getSalario());
 		
 		
 		gerente1.setSenha(552216);
@@ -18,7 +26,7 @@ public class TesteGerente {
 		System.out.println(autenticou);
 		
 		System.out.println(gerente1.getBonificacao());
-		
+		System.out.println(funcionario1.getBonificacao());
 		
 	}
 
